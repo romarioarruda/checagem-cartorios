db5d60a732194f323965e7473d6fed44c6023ada
Romáio Arruda <35845386+romarioarruda@users.noreply.github.com>
2019-10-30 09:35:03 -0300

    Update footer.php

--- views/template/footer.php
+++ views/template/footer.php
@@ -1,8 +1,8 @@
 
-    <script src="../utils/jquery/jquery-3.4.1.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="../utils/jquery/jquery-mask/dist/jquery.mask.min.js"></script>
     <script src="./assets/js/mask.js"></script>
-    <script src="../utils/js/bootstrap.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
     <script src="./assets/js/checarXml.js"></script>
     <script src="./assets/js/atualizarContatos.js"></script>
